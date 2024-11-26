5f792cad81616c53b5b0d0576e48be33e6640373
PBence-keri <pragai.bence-2020@keri.mako.hu>
2024-11-26 10:07:24 +0100

    Linked in home

--- js/app.js
+++ js/app.js
@@ -1,5 +1,47 @@
-angular.module("myApp", ["ngRoute"])
-.controller('mycontroller', ['$routeProvider', function($routeProvider){
-    $routeProvider
-    
-}]);
+;(function(window, angular) {
+
+  'use strict';
+
+  // Application module
+  angular.module('myApp', [
+    'ui.router'
+  ])
+
+  // Application config
+  .config([
+    '$stateProvider', 
+    '$urlRouterProvider', 
+    ($stateProvider, $urlRouterProvider) => {
+
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: './html/home.html',
+          controller: 'homeController'
+        });
+        
+      $urlRouterProvider.otherwise('/');
+    }
+  ])
+
+  // Application run
+  .run([  
+    '$rootScope',
+    ($rootScope) => {
+      $rootScope.feliratkozas = () => {
+        alert('asdasdasd!');
+      }
+    }
+  ])
+
+  // Home controller
+  .controller('homeController', [
+    '$scope',
+    function($scope) {
+      
+    }
+  ])
+
+  
+
+})(window, angular);
